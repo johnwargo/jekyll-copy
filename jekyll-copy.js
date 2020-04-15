eedae579effa1a6048565dc72fc7e6e67d9ee425
John M. Wargo <johnwargo@users.noreply.github.com>
2020-04-15 17:36:25 -0400

    working on validating config

--- jekyll-copy.js
+++ jekyll-copy.js
@@ -13,24 +13,33 @@ https: //stackabuse.com/reading-and-writing-yaml-to-a-file-in-node-js-javascript
  var yaml = require('js-yaml');
 var appName = 'Jekyll Template File Copy';
 var appAuthor = 'by John M. Wargo (johwargo.com)';
+function validConfig() {
+    return false;
+}
+function getTemplateName() {
+    return '';
+}
 function getTemplateFolder() {
     console.log(chalk.yellow('\nValidating template folder'));
 }
-program
-    .version('0.0.1')
-    .command('ls [folder]')
+console.log();
+console.log(boxen(appName + "\n\n" + appAuthor, { padding: 1 }));
+program.version('0.0.1');
+program.command('ls [folder]')
     .description('List Jekyll template folders and files.')
     .action(function (folder) {
     if (folder === void 0) { folder = 'current'; }
     console.log("Listing files for " + folder + " folder");
 });
-program
-    .command('--cp <file>')
+program.command('cp <file>')
     .description('Copy a Jekyll template file to the current location.')
     .action(function (file) {
     console.log('executing cp');
 });
-console.log();
-console.log(boxen(appName + "\n\n" + appAuthor, { padding: 1 }));
-console.log(program.list);
-console.dir(program.commands);
+if (validConfig()) {
+    program.parse(process.argv);
+}
+else {
+    console.log(chalk.red('\nInvalid configuration!'));
+    console.log('Make sure the terminal is in a Jekyll project folder before executing the command');
+}
--- src/jekyll-copy.ts
+++ src/jekyll-copy.ts
@@ -14,30 +14,72 @@ const yaml = require('js-yaml')
 
 const appName = 'Jekyll Template File Copy';
 const appAuthor = 'by John M. Wargo (johwargo.com)';
+const blankStr = '';
+const configFile = '_config.yml';
+const gemFile = 'Gemfile';
+
+function fileExists(fileName: string): boolean {
+  try {
+    return fs.existsSync(fileName);
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+}
+
+function validConfig(): boolean {
+  // looking for two files
+  if (fileExists(configFile) && fileExists(gemFile)) {
+    // Read the template name from the config file
+
+  } else {
+    return false;
+  }
+  return false;
+}
+
+function getTemplateName(): string {
+  try {
+    let fileContents = fs.readFileSync(configFile, 'utf8');
+    let data = yaml.safeLoad(fileContents);
+    if (data) {
+      console.log(data);
+      return data.theme;
+    } else {
+      return blankStr;
+    }
+  } catch (e) {
+    console.error(`Unable to read config file (${configFile})`);
+    return blankStr;
+  }
+}
 
 function getTemplateFolder() {
   console.log(chalk.yellow('\nValidating template folder'));
 
 }
 
-program
-  .version('0.0.1')
-  .command('ls [folder]')
+console.log();
+console.log(boxen(`${appName}\n\n${appAuthor}`, { padding: 1 }));
+
+program.version('0.0.1');
+
+program.command('ls [folder]')
   .description('List Jekyll template folders and files.')
   .action((folder: string = 'current') => {
     console.log(`Listing files for ${folder} folder`);
 
   });
 
-program
-  .command('--cp <file>')
+program.command('cp <file>')
   .description('Copy a Jekyll template file to the current location.')
   .action((file: string) => {
     console.log('executing cp');
   });
 
-console.log();
-console.log(boxen(`${appName}\n\n${appAuthor}`, { padding: 1 }));
-
-// console.log(program.list);
-// console.dir(program.commands);
+if (validConfig()) {
+  program.parse(process.argv);
+} else {
+  console.log(chalk.red('\nInvalid configuration!'));
+  console.log('Make sure the terminal is in a Jekyll project folder before executing the command');
+}
